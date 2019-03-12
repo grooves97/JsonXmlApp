d01ec33daf025a8438a1a245790344b39fdc3de9
Aseka <grooves97@gmail.com>
2019-03-12 07:52:36 +0600

    Json car

---
+++ CarsApp/CarsApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Json;
+using System.Runtime.Serialization;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace CarsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            var vehiсle = new List<Car>();
+
+            DataContractJsonSerializer jsonFormatter = new DataContractJsonSerializer(typeof(List<Car>));
+
+            using (FileStream fs = new FileStream("./Cars.json",FileMode.OpenOrCreate))
+            {
+                //var file = File.OpenRead("Cars.json");
+                List<Car> newVehiсle = (List<Car>)jsonFormatter.ReadObject(fs);
+                newVehiсle.ToString();
+                //string json = JsonConvert.DeserializeObject();
+
+                foreach (var c in newVehiсle)
+                {
+                    car.Show();
+                }
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
